1a2640e9e82a880397ed68ca04a0086d30311063
Aryan Kalpesh Pancholi <aryanpancholi7@gmail.com>
2026-06-18 23:06:36 +0300

    Add: perfect-squares - submission-1

---
+++ Data Structures & Algorithms/perfect-squares/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    int numSquares(int target) {
+        vector<int> dp(target + 1, INT_MAX);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int num = 2; num <= target; num++) {
+            for (int i = 1; i * i <= num; i++) {
+                dp[num] = min(dp[num], 1 + dp[num - (i * i)]);
+            }
+        }
+        return dp[target];
+    }
+};
