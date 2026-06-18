bf869b56871c3d8333f84fb24f61b170fe879a71
Aryan Kalpesh Pancholi <aryanpancholi7@gmail.com>
2026-06-18 23:05:52 +0300

    Add: perfect-squares - submission-0

---
+++ Data Structures & Algorithms/perfect-squares/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSquares(int target) {
+        vector<int> dp(target+1,INT_MAX);
+
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int num=2;num<=target;num++){
+            for(int i=1;i*i<=num;i++){
+                if(dp[num-(i*i)]!=INT_MAX){
+                    dp[num]=min(dp[num],1+dp[num-(i*i)]);
+                }
+            }
+        }
+        return dp[target];
+    }
+};
