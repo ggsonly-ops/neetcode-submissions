e98329f8614c0fed5100f176306518481db46ace
Aryan Kalpesh Pancholi <aryanpancholi7@gmail.com>
2026-07-01 07:35:49 +0530

    Add: implement-stack-using-queues - submission-1

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-1.cpp
@@ -0,0 +1,29 @@
+class MyStack {
+public:
+    queue<int> q;
+
+    MyStack() {}
+
+    void push(int x) {
+        int size = q.size();
+        q.push(x);
+        for (int i = 0; i < size; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int ele = q.front();
+        q.pop();
+        return ele;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
