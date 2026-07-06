5cf8b642e60b82ae459e79bed58170810b602891
Aryan Kalpesh Pancholi <aryanpancholi7@gmail.com>
2026-07-06 14:50:39 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        
+        vector<int> freq(26,0);
+
+        for(char task:tasks){
+            freq[task-'A']++;
+        }
+
+        int maxFreq=*max_element(freq.begin(),freq.end());
+        int maxCount=count(freq.begin(),freq.end(),maxFreq);
+
+        int partCount=maxFreq-1;
+        int partLength=n+1;
+        int emptySlots= partCount * partLength + maxCount;
+
+        return max(int(tasks.size()),emptySlots); 
+    }
+};
