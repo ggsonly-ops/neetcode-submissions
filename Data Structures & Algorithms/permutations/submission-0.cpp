1cf96208c689a8412ad9daea26376d534e046343
Aryan Kalpesh Pancholi <aryanpancholi7@gmail.com>
2026-07-09 23:06:05 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    void recurPermute(int idx, vector<int>& nums, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            recurPermute(idx + 1, nums, ans);
+            swap(nums[idx], nums[i]); // undo the swap -- backtrack
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        recurPermute(0, nums, ans);
+        return ans;
+    }
+};
